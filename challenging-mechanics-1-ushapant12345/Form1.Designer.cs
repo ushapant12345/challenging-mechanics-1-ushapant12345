32757e412884b62fdb8f957c3d400e49838a18c5
LabUser <LabUser@SU304_10>
2017-09-15 21:13:06 -0600

    I have completed challenging mechanics 1 assignment.

--- challenging-mechanics-1-ushapant12345/Form1.Designer.cs
+++ challenging-mechanics-1-ushapant12345/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(69, 103);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.Text = "Enter first name";
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(69, 155);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(100, 20);
+            this.txtLastname.TabIndex = 1;
+            this.txtLastname.Text = "Enter last name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Press Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- challenging-mechanics-1-ushapant12345/Form1.cs
+++ challenging-mechanics-1-ushapant12345/Form1.cs
@@ -13,8 +13,62 @@ namespace challenging_mechanics_1_ushapant12345
     public partial class Form1 : Form
     {
         public Form1()
+
+       
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+            MessageBox.Show(txtFirstName.Text);
+            MessageBox.Show(txtLastname.Text);
+            MessageBox.Show(txtFirstName.Text + "-");
+            MessageBox.Show(txtFirstName.Text + "- " +  "12345");
+            MessageBox.Show(txtFirstName.Text + "-" + "12345" + "-");
+            MessageBox.Show(txtFirstName.Text + "-" + "*" + "-" + "*" + "-" + "*" + "-");
+            MessageBox.Show(txtFirstName.Text + "-" + "*" + "-" + "*" + "-" + "*" + "-" + txtLastname.Text);
+            MessageBox.Show(txtFirstName.Text + " " + txtLastname.Text);
+            MessageBox.Show(txtFirstName.Text + " " + txtFirstName.Text);
+            MessageBox.Show(txtFirstName.Text + " " + txtFirstName.Text + " " + txtLastname.Text + " " + txtLastname.Text);
+            MessageBox.Show(txtFirstName.Text + "-" + txtLastname.Text);
+            MessageBox.Show(txtFirstName.Text + "-" + txtLastname.Text + "*");
+            MessageBox.Show(txtLastname.Text + "," + txtFirstName.Text);
+            MessageBox.Show(txtFirstName.Text + "\r\n" + txtLastname.Text);
+            MessageBox.Show("First Name:"+ txtFirstName.Text + "\r\n " + "Last Name:" + txtLastname.Text);
+            MessageBox.Show("First---Last" + "\r\n" + txtFirstName.Text + "---" + txtLastname.Text);
+            MessageBox.Show(String.Format(txtFirstName.Text + "\r\n" + "{0}", txtLastname.Text));
+            MessageBox.Show(String.Format("Firstname:" + txtFirstName.Text + "\r\n" + "Lastname:" + "{0}", txtLastname.Text));
+            MessageBox.Show(String.Format("First---Last" + "\r\n" + txtFirstName.Text + "{0}", txtLastname.Text));
+            MessageBox.Show(String.Format(txtFirstName.Text + "-" + "*" + "-" + "*" + "-" + "*" + "-" + "{0}", txtLastname.Text));
+            MessageBox.Show(String.Format(txtFirstName.Text + "-" + "*" + "-" + "*" + "-" + "*" + "-" + "{0}", txtLastname.Text));
+
+
+
+
+
+
+
+            //fill in the blanks
+            //MessageBox.Show(string.Format(("{ 0}" + "{-}" + "{2}" + "{-}" + "{2}" + "{-}" + "{2}" + "{-}" + "{1}"), "A", "B", "*"));
+            //MessageBox.Show(string.Format(("{ 1}" + "{0}" + "{3}" + "{0}" + "{3}" + "{0}" + "{3}" + "{0}" + "{2}"), "-", "A", "B", "*"));
+            //MessageBox.Show(string.Format(("{ 1}" + "{3}" + "{0}" + "{3}" + "{0}" + "{3}" + "{0}" + "{3}" + "{0}" + "2"), "-", "A", "B", "\r\n"));
+            //MessageBox.Show(string.Format(("{ 0}" + "{-}" + "{1}" + "{-}" + "{0}" + "{-}" + "{1}" + "{-}" + "{0}"), "A","B"));
+            //MessageBox.Show(string.Format(("{ 0}" + "{2}" + "{1}" + "{2}" + "{0}" + "{2}" + "{1}" + "{2}" + "{0}"), "A", "B", "-"));
+
+
+
+
+
+
+
+        }
+
+
+
+
+
     }
 }
